25677b2a0ab5b322b0889b76df99246480108ff5
talabidele <talabibamidele7@gmail.com>
2022-07-13 11:06:23 +0100

    corrected window.localStorage

--- components/hooks/useLocalStorage.js
+++ components/hooks/useLocalStorage.js
@@ -26,7 +26,7 @@ function useLocalStorage(key, initialValue) {
       // Save state
       setStoredValue(valueToStore);
       // Save to local storage
-      windowlocalStorage.setItem(key, JSON.stringify(valueToStore));
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
     } catch (error) {
       // A more advanced implementation would handle the error case
       console.log(error);
