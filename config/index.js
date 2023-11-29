bef310c267b042f2f873364ea56e338e3c7a9b7d
talabidele <talabibamidele7@gmail.com>
2023-11-29 02:46:42 +0100

    Changed url

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 // export const API_URL = process.env.API_URL
-export const API_URL = 'http://jesseremedies.com.ng/api'
+export const API_URL = 'https://jesseremedies.com.ng/api'
 
 export const NEXT_PUBLIC_URL =
 	process.env.NEXT_PUBLIC_URL || 'http://localhost:3000'
