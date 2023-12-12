c7bf03adeccdb725ec55ee3f8ca42e7bb3c12604
talabidele <talabibamidele7@gmail.com>
2023-12-12 15:46:28 +0100

    Testing key

--- config/index.js
+++ config/index.js
@@ -4,4 +4,4 @@ export const API_URL = process.env.NEXT_PUBLIC_API_URL
 export const PAYSTACK_KEY = process.env.NEXT_PUBLIC_PAYSTACK_KEY
 
 export const NEXT_PUBLIC_URL =
-	process.env.NEXT_PUBLIC_URL || 'http://localhost:3001'
+	process.env.NEXT_PUBLIC_URL || 'http://localhost:3000'
